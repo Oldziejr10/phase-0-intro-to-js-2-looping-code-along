eee9dd285d8474cf9fc9097e1f38e10b4426646a
Rafal Oldziej <rafal.oldziej1@gmail.com>
2022-05-27 22:13:04 -0400

    commit tree ce74dbde74ed3406fc4ca7fa145a5f26e5221f21

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+
+
+function writeCards(nameForCards, suprise) { 
+
+    const newArray = []
+
+    for (let i = 0; i < nameForCards.length; i++) {
+        newArray.push(`Thank you, ${nameForCards[i]}, for the wonderful ${suprise} gift!`);
+    }
+
+    return newArray
+
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "suprise");
+
+function countDown() { 
+    let count = 0;
+while (count < 11) {
+  console.log(count++);
+}
+}
